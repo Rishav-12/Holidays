750948eb4a98dfa36acd2b076733ee6f9811d2bb
Rishav <rishavmitra10@gmail.com>
2021-03-17 11:22:53 +0530

    created main script & README file

---
+++ README.md
@@ -0,0 +1,12 @@
+# Holidays
+A fun Python script that uses an api to get you the holidays of any place you want on a given day
+
+### Description
+* It uses an api call to fetch the data
+* Then it transforms it into a json and parses it to get useful information
+* Finally it serves that information to the user
+
+### How to use
+* Generate an api key from calendarific.com
+* Create a file *api_key.txt*, put your api key in and keep it beside the python script
+* Run the script from your console
---
+++ main.py
@@ -0,0 +1,41 @@
+'''
+This script uses the https://calendarific.com/ api
+'''
+import requests
+from datetime import date
+
+with open('api_key.txt', 'r') as key: # <-- use your own api key here
+	api_key = key.read().strip()
+
+country = 'IN' # <-- you can also change the country... just look up the iso code from the docs
+
+now = date.today()
+day = str(now.day)
+month = str(now.month)
+year = str(now.year)
+
+url = f'https://calendarific.com/api/v2/holidays?&api_key={api_key}&country={country}&day={day}&month={month}&year={year}'
+
+try:
+	r = requests.get(url)
+	data = r.json()
+
+	holidays = data['response']['holidays']
+
+except Exception:
+	holidays = None
+
+if holidays is None:
+	print("Could not fetch data. Terminating")
+
+elif len(holidays) == 0:
+	print("No holidays today...")
+
+else:
+	print("\nToday's holidays :")
+	print("*************************")
+
+	for holiday in holidays:
+		print("\n")
+		print(holiday['name'])
+		print(holiday['description'])
